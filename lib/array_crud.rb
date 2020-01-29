c1ed78f2393c53b30a1b199457522d188fb5813a
Kyle Talmage <ktalmage85@gmail.com>
2020-01-29 17:45:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,30 +1,53 @@
 def create_an_empty_array
-  
+   []
 end
 
-def create_an_array
+p create_an_empty_array
   
+def create_an_array
+  create_an_array = ["Kyle", "Kelly", "Elizabeth", "James"]
 end
+
+puts create_an_array
+
 
 def add_element_to_end_of_array(array, element)
-  
-end
+   array << element
+
+ end
+
+   array = ["Kyle", "Elizabeth", "Kaity","Dad"]
+   element = "Mom"
+   puts add_element_to_end_of_array(array,element)
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
+  
+  array = ["Kyle", "Kaity", "Elizabeth", "Dad"]
+  element = "Kelly"
+  puts add_element_to_start_of_array(array,element)
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
+  array = ["Dad","Kyle","Mom","Kaity"]
+  puts remove_element_from_end_of_array(array)
 
 def remove_element_from_start_of_array(array)
+  array.shift
+  end
   
-end
+  array = ["Elizabeth","Mom","Kaity","James"]
+  puts remove_element_from_start_of_array(array)
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
+  
+  array = ["Elizabeth","Kyle","Kaity","James"]
+  index_number = 0
+  puts retrieve_element_from_index(array, index_number)
 
 def retrieve_first_element_from_array(array)
   
